906c21cb2c25e05b6b692d97557ed30523de2ca3
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-30 15:57:37 +0200

    Init Project: created file php & input

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    
+
+    <form action="paragrafo.php">
+        <input placeholder="parola" type="text">
+        <input placeholder="parola da censurare" type="text">
+
+        <button type="submit">Invia</button>
+
+
+    </form>
+    
+</body>
+</html>
